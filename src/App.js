d1940ba08d6223bab93bc87c793acae24b620f44
midou95 <midouchecha@gmail.com>
2020-11-28 17:49:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import  './compoent/profile';
+function App() {
+  return (
+    <>
+<pf/>
+<myname/>
+<address/>
+
+    
+    
+    </>
+  );
+}
+
+export default App;
